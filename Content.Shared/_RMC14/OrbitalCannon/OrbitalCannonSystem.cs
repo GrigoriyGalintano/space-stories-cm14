bc77d74fbde7dff31dd7296ca34d3afbf5174c06
GrigoriyGalintano <code.grig@mail.ru>
2026-06-06 23:23:49 +0300

    Fix OB tray stuck after hive core block

--- Content.Shared/_RMC14/OrbitalCannon/OrbitalCannonSystem.cs
+++ Content.Shared/_RMC14/OrbitalCannon/OrbitalCannonSystem.cs
@@ -668,16 +668,19 @@ public sealed class OrbitalCannonSystem : EntitySystem
 
                 cannon.Comp.Status = OrbitalCannonStatus.Unloaded;
                 cannon.Comp.LastFireAt = time;
+                cannon.Comp.UnloadingTrayAt = time;
                 Dirty(cannon, cannon.Comp);
-
-                var cannonEnt = new Entity<OrbitalCannonComponent>(cannon, cannon.Comp);
-                CannonStatusChanged(cannonEnt);
 
                 if (_container.TryGetContainer(trayId, tray.FuelContainer, out var fuelCont))
                     _container.CleanContainer(fuelCont);
 
                 if (_container.TryGetContainer(trayId, tray.WarheadContainer, out var warheadCont))
                     _container.CleanContainer(warheadCont);
+
+                _animation.TryFlick(cannon.Owner, cannon.Comp.UnloadingAnimation, cannon.Comp.UnloadedState, cannon.Comp.BaseLayerKey);
+
+                var cannonEnt = new Entity<OrbitalCannonComponent>(cannon, cannon.Comp);
+                CannonStatusChanged(cannonEnt);
 
                 return false;
             }
