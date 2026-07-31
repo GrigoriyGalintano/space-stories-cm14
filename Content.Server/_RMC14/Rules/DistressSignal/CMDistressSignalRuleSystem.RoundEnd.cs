dd7bf897940456f0e340713d41f44ad33ed443c1
GrigoriyGalintano <156821788+GrigoriyGalintano@users.noreply.github.com>
2026-07-31 10:52:42 +0300

    Окно для призраков в стиле CMSS13 (#477)

--- Content.Server/_RMC14/Rules/DistressSignal/CMDistressSignalRuleSystem.RoundEnd.cs
+++ Content.Server/_RMC14/Rules/DistressSignal/CMDistressSignalRuleSystem.RoundEnd.cs
@@ -90,6 +90,7 @@ public sealed partial class CMDistressSignalRuleSystem
 
     private void UpdateHijackState(CMDistressSignalRuleComponent distress)
     {
+        var wasEndgame = IsDistressEndgameActive();
         var hijack = false;
         var dropshipQuery = EntityQueryEnumerator<DropshipComponent>();
         while (dropshipQuery.MoveNext(out var dropship))
@@ -103,6 +104,7 @@ public sealed partial class CMDistressSignalRuleSystem
         {
             distress.Hijack = true;
             distress.AbandonedAt ??= time + distress.AbandonedDelay;
+            RaiseEndgameChanged(wasEndgame);
         }
     }
 
@@ -388,6 +390,13 @@ public sealed partial class CMDistressSignalRuleSystem
 
     private void OnCompRemove<T>(Entity<T> ent, ref ComponentRemove args) where T : IComponent?
     {
+        CheckRoundShouldEnd();
+    }
+
+    private void OnXenoComponentRemoved(Entity<XenoComponent> ent, ref ComponentRemove args)
+    {
+        var ev = new XenoComponentChangedEvent(ent);
+        RaiseLocalEvent(ref ev);
         CheckRoundShouldEnd();
     }
 
